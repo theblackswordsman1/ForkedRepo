eb21314448b2aede048c149a8647ccbf55949b16
Aron William Bechen <144605848+theblackswordsman1@users.noreply.github.com>
2024-03-05 20:39:20 +0100

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text>lol.</Text>
+      <Text>My second line of text.</Text>
       <StatusBar style="auto" />
     </View>
   );
